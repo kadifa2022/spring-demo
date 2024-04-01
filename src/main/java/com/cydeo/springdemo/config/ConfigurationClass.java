bba643532833080cba6e6a15e7eb82f0b7bac9f9
Kadifa <Kudickadifa@gmail.com>
2024-04-01 16:11:57 -0500

    practice

---
+++ src/main/java/com/cydeo/springdemo/config/ConfigurationClass.java
@@ -0,0 +1,13 @@
+package com.cydeo.springdemo.config;
+
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigurationClass {
+//    @Bean
+//    public InMemoryHttpExchangeRepository httpTraceRepository(){
+//        return new InMemoryHttpExchangeRepository();
+//    }
+}
